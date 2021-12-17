0a5ef49f9d874f6542f8864a70043dd7ef03a0bd
lucas <lucascabraldevv@gmail.com>
2021-12-17 20:48:15 -0300

    TakePicture added.

--- app/src/main/java/com/lucascabral/mycameraxapp/ResultTakePictureActivity.kt
+++ app/src/main/java/com/lucascabral/mycameraxapp/ResultTakePictureActivity.kt
@@ -1,16 +1,55 @@
 package com.lucascabral.mycameraxapp
 
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
+import androidx.lifecycle.lifecycleScope
 import com.lucascabral.mycameraxapp.databinding.ActivityResultTakePictureBinding
+import java.io.File
 
 class ResultTakePictureActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityResultTakePictureBinding
+    private var latestTmpUri: Uri? = null
+    private val takeImageResult = registerForActivityResult(
+        ActivityResultContracts.TakePicture()) { isSuccess ->
+        if (isSuccess) {
+            latestTmpUri?.let { uri ->
+                binding.imagePreview.setImageURI(uri)
+            }
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityResultTakePictureBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        setClickListeners()
+    }
+
+    private fun setClickListeners() {
+        binding.takeImageButton.setOnClickListener {
+            takeImage()
+        }
+    }
+
+    private fun takeImage() {
+        lifecycleScope.launchWhenCreated {
+            getTmpFileUri().let { uri ->
+                latestTmpUri = uri
+                takeImageResult.launch(uri)
+            }
+        }
+    }
+
+    private fun getTmpFileUri(): Uri {
+        val tmpFile = File.createTempFile("tmp_image_file", ".png", cacheDir).apply {
+            createNewFile()
+            deleteOnExit()
+        }
+        return FileProvider.getUriForFile(applicationContext, "${BuildConfig.APPLICATION_ID}.provider", tmpFile)
     }
 }
